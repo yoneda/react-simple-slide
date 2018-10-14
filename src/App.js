7dfe4418bcc8cd2a920ee1e6ae454f65cdde75bf
yoneda <nsshowdream@gmail.com>
2018-10-14 12:57:19 +0900

    add Slide Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './Menu.js';
+import Slide from './Slide.js';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Menu onClickNext={()=>console.log("aa")} onClickPrev={()=>console.log("bb")}/>
+        <Slide text={"hello"} />
       </div>
     );
   }
---
+++ src/Slide.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Slide.css';
+
+const Slide = ({text}) => (
+  <div>
+    <p>{text}</p>
+  </div>
+)
+
+export default Slide;

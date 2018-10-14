8d63629590782bc66d1811e81f7963a6999762da
yoneda <nsshowdream@gmail.com>
2018-10-14 13:31:02 +0900

    make it possible to change slides

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import Menu from './Menu.js';
 import Slide from './Slide.js';
 import './App.css';
 
+let state = {
+  pageNum: 0,
+  slides: ["aaa","bbb","ccc","ddd"]
+}
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pageNum: 0,
+      slides: ["aaa","bbb","ccc","ddd"]
+    }
+    this.addPageNum = this.addPageNum.bind(this);
+    this.subPageNum = this.subPageNum.bind(this);
+  }
+  addPageNum(){
+    const nowPageNum = this.state.pageNum;
+    this.setState({pageNum:nowPageNum+1})
+  }
+  subPageNum(){
+    const nowPageNum = this.state.pageNum;
+    this.setState({pageNum:nowPageNum-1})
+  }
   render() {
     return (
       <div className="App">
-        <Menu onClickNext={()=>console.log("aa")} onClickPrev={()=>console.log("bb")}/>
-        <Slide text={"hello"} />
+        <Menu onClickNext={this.addPageNum} onClickPrev={this.subPageNum}/>
+        <Slide text={this.state.slides[this.state.pageNum]}/>
       </div>
     );
   }

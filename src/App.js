a3f7249c86839ced73c7f6239431fe7e3cf58bfa
yoneda <nsshowdream@gmail.com>
2018-10-18 00:37:18 +0900

    add ProgressBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import Menu from './Menu.js';
 import Slide from './Slide.js';
+import ProgressBar from './ProgressBar.js';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
+    const slides = ["aaa","bbb","ccc","ddd"];
     this.state = {
       pageNum: 0,
-      slides: ["aaa","bbb","ccc","ddd"]
+      pageMax: slides.length,
+      slides: slides
     }
     this.addPageNum = this.addPageNum.bind(this);
     this.subPageNum = this.subPageNum.bind(this);
     this.onKeyPressed = this.onKeyPressed.bind(this);
+    this.calcParcentage = this.calcParcentage.bind(this);
   }
   componentDidMount(){
     document.addEventListener("keydown",this.onKeyPressed);
@@ -23,6 +27,7 @@ class App extends Component {
   addPageNum(){
     const nowPageNum = this.state.pageNum;
     this.setState({pageNum:nowPageNum+1})
+    console.log(this.state);
   }
   subPageNum(){
     const nowPageNum = this.state.pageNum;
@@ -32,11 +37,17 @@ class App extends Component {
     if(event.key === "ArrowRight") this.addPageNum();
     else if(event.key === "ArrowLeft") this.subPageNum();
   }
+  calcParcentage(num,maxNum){
+    const deci = parseFloat(num)/parseFloat(maxNum);
+    const parcentage = Math.round(deci*100);
+    return parcentage;
+  }
   render() {
     return (
       <div className="App" onKeyDown={this.onKeyPressed} tabIndex="0">
         <Menu onClickNext={this.addPageNum} onClickPrev={this.subPageNum}/>
         <Slide text={this.state.slides[this.state.pageNum]}/>
+        <ProgressBar parcentage={this.calcParcentage(this.state.pageNum+1,this.state.pageMax)}/>
       </div>
     );
   }
